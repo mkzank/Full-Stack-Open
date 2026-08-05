4620f57578f0d343973a915578a3d4ae66499e3c
CeliaLty <1155192155@link.cuhk.edu.hk>
2026-08-05 22:53:09 +0800

    part1 exercise 1.6: unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,70 +1,32 @@
+import { useState } from 'react'
+
+const Header = ({headerText}) => (<h1> {headerText} </h1>)
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}> 
+    {text}
+  </button>
+)
+const ShowStatistics = ({text, number}) => <p> {text} {number}</p>
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    const Header = (props) => {
-      return (
-        <>
-          <h1>
-            {props.course.name}
-          </h1>
-        </>
-      )
-  }
-
-  const Part = (props) => {
-    console.log(props)
-      return (
-        <>
-          <p>
-            {props.name} {props.exercises}
-          </p>
-        </>
-      )
-  }
-
-  const Content = () => {
-    return (
-      <>
-        <Part name={course.parts[0].name} exercises={course.parts[0].exercises} />
-        <Part name={course.parts[1].name} exercises={course.parts[1].exercises} />
-        <Part name={course.parts[2].name} exercises={course.parts[2].exercises} />
-      </>
-    )
-  }
-
-  const Total = () => {
-    return (
-      <>
-        <p>
-          Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-        </p>
-      </>
-    )
-  }
 
   return (
     <div>
-      <Header course={course} /> 
-      <Content />
-      <Total />
+      <Header headerText="give feedback"></Header>
+      <Button onClick={() => setGood(good + 1)} text="good"> </Button>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"> </Button>
+      <Button onClick={() => setBad(bad + 1)} text="bad"> </Button>
+      <Header headerText="statistics"> </Header>
+      <ShowStatistics text="good" number={good}></ShowStatistics>
+      <ShowStatistics text="neutral" number={neutral}></ShowStatistics>
+      <ShowStatistics text="bad" number={bad}></ShowStatistics>
     </div>
   )
-} 
+}
 
 export default App
