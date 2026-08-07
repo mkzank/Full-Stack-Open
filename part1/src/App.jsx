4e461646bc4e12f06c98fb706ab95ddea6bb841a
CeliaLty <1155192155@link.cuhk.edu.hk>
2026-08-08 00:54:50 +0800

    1.7 unitcafe step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,23 +8,61 @@ const Button = ({onClick, text}) => (
 )
 const ShowStatistics = ({text, number}) => <p> {text} {number}</p>
 
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleStatistics = (typeOfStat) => {
+    return () => {
+      if (typeOfStat == "good") {
+        const updatedGood = good + 1
+        const updatedAll = updatedGood + neutral + bad
+        setGood(updatedGood)
+        setAll(updatedAll)
+        setAverage((updatedGood - bad) / updatedAll)
+        setPositive((updatedGood / updatedAll) * 100)
+      }
+      else if (typeOfStat == "neutral") {
+        const updatedNeutral = neutral + 1
+        const updatedAll = good + updatedNeutral + bad
+        setNeutral(updatedNeutral)
+        setAll(updatedAll) 
+        setAverage((good - bad) / updatedAll)
+        setPositive((good / updatedAll) * 100)
+      }
+      else {
+        const updatedBad = bad + 1
+        const updatedAll = good + neutral + updatedBad
+        setBad(updatedBad)
+        setAll(updatedAll)
+        setAverage((good - updatedBad) / updatedAll)
+        setPositive((good / updatedAll) * 100)
+      }
+    }
+  }
+
 
   return (
     <div>
       <Header headerText="give feedback"></Header>
-      <Button onClick={() => setGood(good + 1)} text="good"> </Button>
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"> </Button>
-      <Button onClick={() => setBad(bad + 1)} text="bad"> </Button>
+      <Button onClick={handleStatistics("good")} text="good"> </Button>
+      <Button onClick={handleStatistics("neutral")} text="neutral"> </Button>
+      <Button onClick={handleStatistics("bad")} text="bad"> </Button>
       <Header headerText="statistics"> </Header>
       <ShowStatistics text="good" number={good}></ShowStatistics>
       <ShowStatistics text="neutral" number={neutral}></ShowStatistics>
       <ShowStatistics text="bad" number={bad}></ShowStatistics>
+      <ShowStatistics text="all" number={all}></ShowStatistics>
+      <ShowStatistics text="average" number={average}></ShowStatistics>
+      <ShowStatistics text="positive" number={positive}></ShowStatistics>
     </div>
   )
 }
